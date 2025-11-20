37545e905a8159d80a88e0ab2817af6facb6b531
UbheAbhe <omoranmore@gmail.com>
2025-11-20 17:54:13 -0500

    Anuncio

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-    res.send('¡Hola desde Render y GitHub Actions!');
+    res.send('Zoom a las 6:02PM, entren csmr');
 });
 
 app.listen(port, () => {
