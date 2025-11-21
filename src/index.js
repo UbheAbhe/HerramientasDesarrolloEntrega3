e51bbcb7a4b07aa77522a20b8d5a33a8cd00133c
UbheAbhe <omoranmore@gmail.com>
2025-11-20 19:14:47 -0500

    V.3.2

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ app.get('/', (req, res) => {
             <meta charset="UTF-8">
             <meta name="viewport" content="width=device-width, initial-scale=1.0">
             <title>Herramientas de Desarrollo - Presentación Profesional</title>
-            <link rel="stylesheet" href="/src/public/styles.css"> 
+            <link rel="stylesheet" href="/styles.css"> 
             <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;700&display=swap" rel="stylesheet">
         </head>
         <body>
